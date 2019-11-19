476b7b9860d766089dff3a0b45caa50eee83d601
xiayubin <xiayubin@patsnap.com>
2019-11-19 18:29:01 +0800

    update kp20k train scripts

--- scripts/train_copyrnn_kp20k.sh
+++ scripts/train_copyrnn_kp20k.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 ROOT_PATH=${PWD}
 DATA_DIR=$ROOT_PATH/data
-SRC_FILENAME=$DATA_DIR/kp20k_train.jsonl
+TRAIN_FILENAME=$DATA_DIR/kp20k.train.jsonl
+VALID_FILENAME=$DATA_DIR/kp20k.valid.jsonl
+TEST_FILENAME=$DATA_DIR/kp20k.test.jsonl
 VOCAB_PATH=$DATA_DIR/vocab_kp20k.txt
 DEST_DIR=$DATA_DIR/kp20k/
+EXP_NAME=copyrnn_basic
+
 #export CUDA_VISIBLE_DEVICES=1
 
-python3 deep_keyphrase/copy_rnn/train.py -src_filename $SRC_FILENAME \
-  -vocab_path $VOCAB_PATH -dest_dir $DEST_DIR
+python3 deep_keyphrase/copy_rnn/train.py -exp_name $EXP_NAME \
+  -train_filename $TRAIN_FILENAME \
+  -valid_filename $VALID_FILENAME -test_filename $TEST_FILENAME \
+  -vocab_path $VOCAB_PATH -dest_dir $DEST_DIR \
+  -bidirectional -teacher_forcing -copy_net
