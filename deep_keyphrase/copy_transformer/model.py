9adcebdc00da3a506a78fca547f8790e8107edbb
xiayubin <xiayubin@patsnap.com>
2019-11-19 18:23:07 +0800

    fix bug in CopyTransformer

--- deep_keyphrase/copy_transformer/model.py
+++ deep_keyphrase/copy_transformer/model.py
@@ -3,54 +3,71 @@ import torch
 import torch.nn as nn
 from torch.nn.modules.transformer import (TransformerEncoder, TransformerDecoder,
                                           TransformerEncoderLayer, TransformerDecoderLayer)
-from deep_keyphrase.dataloader import TOKENS, TOKENS_LENS, TOKENS_OOV, UNK_WORD
+from deep_keyphrase.dataloader import TOKENS, TOKENS_LENS, TOKENS_OOV, UNK_WORD, PAD_WORD, OOV_COUNT
 
 
 def get_position_encoding(input_tensor, position, dim_size):
-    batch_size, seq_len = input_tensor.size()
-    half_len = seq_len // 2 + seq_len % 2
-    sin_idx = torch.arange(half_len, dtype=torch.float).expand(batch_size, half_len) * 2
-    cos_idx = sin_idx + 1
-    sin_embed = torch.sin(position / torch.pow(10000, sin_idx / dim_size))
-    cos_embed = torch.sin(position / torch.pow(10000, cos_idx / dim_size))
-    position_embed = torch.stack([sin_embed, cos_embed], dim=2).view(batch_size, -1)
-    # truncate the real embedding when seq_len is odd
-    position_embed = position_embed[:, :seq_len, :]
+    assert dim_size % 2 == 0
+    batch_size = len(input_tensor)
+    num_timescales = dim_size // 2
+    time_scales = torch.arange(0, position + 1, dtype=torch.float).unsqueeze(1)
+    dim_scales = torch.arange(0, num_timescales, dtype=torch.float).unsqueeze(0)
+    dim_val = torch.pow(1.0e4, 2 * dim_scales / dim_size)
+    matrix = torch.matmul(time_scales, 1.0 / dim_val)
+    position_embed = torch.cat([torch.sin(matrix), torch.cos(matrix)], dim=1).repeat(batch_size, 1, 1)
+
+    if torch.cuda.is_available():
+        position_embed = position_embed.cuda()
+
     return position_embed
 
 
 class CopyTransformer(nn.Module):
-    def __init__(self, input_dim, src_head_size, target_head_size, feed_forward_dim,
-                 src_dropout, target_dropout, src_layers, target_layers, target_max_len):
+    def __init__(self, args, vocab2id):
         super().__init__()
-        self.encoder = CopyTransformerEncoder(input_dim=input_dim,
-                                              head_size=src_head_size,
-                                              feed_forward_dim=feed_forward_dim,
-                                              dropout=src_dropout,
-                                              num_layers=src_layers)
-        self.decoder = CopyTransformerDecoder(input_dim=input_dim,
-                                              head_size=target_head_size,
-                                              feed_forward_dim=feed_forward_dim,
-                                              dropout=target_dropout,
-                                              num_layers=target_layers,
-                                              target_max_len=target_max_len)
+        embedding = nn.Embedding(len(vocab2id), args.input_dim, vocab2id[PAD_WORD])
+        self.encoder = CopyTransformerEncoder(embedding=embedding,
+                                              input_dim=args.input_dim,
+                                              head_size=args.src_head_size,
+                                              feed_forward_dim=args.feed_forward_dim,
+                                              dropout=args.src_dropout,
+                                              num_layers=args.src_layers)
+        self.decoder = CopyTransformerDecoder(embedding=embedding,
+                                              input_dim=args.input_dim,
+                                              head_size=args.target_head_size,
+                                              feed_forward_dim=args.feed_forward_dim,
+                                              dropout=args.target_dropout,
+                                              num_layers=args.target_layers,
+                                              target_max_len=args.max_target_len,
+                                              vocab2id=vocab2id,
+                                              max_oov_count=args.max_oov_count)
 
     def forward(self, src_dict, prev_output_tokens, encoder_output, encoder_mask,
-                prev_hidden_state, position):
+                prev_decoder_state, position, prev_copy_state):
+        if torch.cuda.is_available():
+            src_dict[TOKENS] = src_dict[TOKENS].cuda()
+            src_dict[TOKENS_LENS] = src_dict[TOKENS_LENS].cuda()
+            src_dict[TOKENS_OOV] = src_dict[TOKENS_OOV].cuda()
+            src_dict[OOV_COUNT] = src_dict[OOV_COUNT].cuda()
+            prev_output_tokens = prev_output_tokens.cuda()
+            prev_decoder_state = prev_decoder_state.cuda()
         if encoder_output is None:
             encoder_output, encoder_mask = self.encoder(src_dict=src_dict)
         output = self.decoder(prev_output_tokens=prev_output_tokens,
-                              prev_hidden_state=prev_hidden_state,
+                              prev_decoder_state=prev_decoder_state,
                               position=position,
                               encoder_output=encoder_output,
                               encoder_mask=encoder_mask,
-                              src_dict=src_dict)
+                              src_dict=src_dict,
+                              prev_copy_state=prev_copy_state)
         return output
 
 
 class CopyTransformerEncoder(nn.Module):
-    def __init__(self, input_dim, head_size, feed_forward_dim, dropout, num_layers):
+    def __init__(self, embedding, input_dim, head_size,
+                 feed_forward_dim, dropout, num_layers):
         super().__init__()
+        self.embedding = embedding
         layer = TransformerEncoderLayer(d_model=input_dim,
                                         nhead=head_size,
                                         dim_feedforward=feed_forward_dim,
@@ -59,26 +76,39 @@ class CopyTransformerEncoder(nn.Module):
 
     def forward(self, src_dict):
         batch_size, max_len = src_dict[TOKENS].size()
-        mask = torch.arange(max_len).expand(batch_size, max_len) < src_dict[TOKENS_LENS]
-        output = self.encoder(src_dict[TOKENS], mask)
-        return output, mask
+        # mask_range = torch.arange(max_len).expand(batch_size, max_len)
+        mask_range = torch.zeros(batch_size, max_len, dtype=torch.bool)
+        if torch.cuda.is_available():
+            mask_range = mask_range.cuda()
+        # mask = mask_range > src_dict[TOKENS_LENS].unsqueeze(1)
+        src_embed = self.embedding(src_dict[TOKENS]).transpose(1, 0)
+        # print(src_embed.size(), mask.size())
+        output = self.encoder(src_embed).transpose(1, 0)
+        return output, mask_range
 
 
 class CopyTransformerDecoder(nn.Module):
-    def __init__(self, input_dim, head_size, feed_forward_dim, dropout, num_layers, target_max_len):
+    def __init__(self, embedding, input_dim, vocab2id, head_size, feed_forward_dim,
+                 dropout, num_layers, target_max_len, max_oov_count):
         super().__init__()
+        self.embedding = embedding
+        self.vocab_size = embedding.num_embeddings
+        self.vocab2id = vocab2id
         layer = TransformerDecoderLayer(d_model=input_dim,
                                         nhead=head_size,
                                         dim_feedforward=feed_forward_dim,
                                         dropout=dropout)
         self.decoder = TransformerDecoder(decoder_layer=layer, num_layers=num_layers)
         self.input_copy_proj = nn.Linear(input_dim, input_dim, bias=False)
+        self.copy_proj = nn.Linear(input_dim, input_dim, bias=False)
+        self.embed_proj = nn.Linear(2 * input_dim, input_dim, bias=False)
         self.generate_proj = nn.Linear(input_dim, self.vocab_size, bias=False)
         self.input_dim = input_dim
         self.target_max_len = target_max_len
+        self.max_oov_count = max_oov_count
 
-    def forward(self, prev_output_tokens, prev_hidden_state, position,
-                encoder_output, encoder_mask, src_dict):
+    def forward(self, prev_output_tokens, prev_decoder_state, position,
+                encoder_output, encoder_mask, src_dict, prev_copy_state):
         src_tokens = src_dict[TOKENS]
         src_tokens_with_oov = src_dict[TOKENS_OOV]
         batch_size, src_max_len = src_tokens.size()
@@ -86,24 +116,32 @@ class CopyTransformerDecoder(nn.Module):
         if torch.cuda.is_available():
             prev_output_tokens = prev_output_tokens.cuda()
         copy_state = self.get_attn_read_input(encoder_output,
-                                              prev_hidden_state,
-                                              prev_output_tokens,
+                                              prev_decoder_state,
+                                              prev_output_tokens[:, -1:],
                                               src_tokens_with_oov,
-                                              src_max_len)
+                                              src_max_len,
+                                              prev_copy_state)
         # map copied oov tokens to OOV idx to avoid embedding lookup error
         prev_output_tokens[prev_output_tokens >= self.vocab_size] = self.vocab2id[UNK_WORD]
         token_embed = self.embedding(prev_output_tokens)
-        pos_embed = get_position_encoding(prev_output_tokens, position, self.input_dim)
+        pos_embed = get_position_encoding(token_embed, position, self.input_dim)
+        # B x seq_len x H
         src_embed = token_embed + pos_embed
-        decoder_input = torch.cat([src_embed, copy_state], dim=2)
+        # print(token_embed.size(),pos_embed.size())
+        # print(src_embed.size(),copy_state.size())
+        decoder_input = self.embed_proj(torch.cat([src_embed, copy_state], dim=2)).transpose(1, 0)
+        # print(decoder_input.size())
         decoder_input_mask = torch.triu(torch.ones(self.input_dim, self.input_dim), 1)
-        # B x decoder_input_seq_len x H
-        decoder_output = self.decoder(tgt=decoder_input, memory=encoder_output,
-                                      tgt_mask=decoder_input_mask, memory_mask=encoder_mask)
+        # B x seq_len x H
+        decoder_output = self.decoder(tgt=decoder_input, memory=encoder_output.transpose(1, 0), )
+        decoder_output = decoder_output.transpose(1, 0)
+        # tgt_mask=decoder_input_mask, memory_mask=encoder_mask)
         # B x 1 x H
         decoder_output = decoder_output[:, -1:, :]
         generation_logits = self.generate_proj(decoder_output).squeeze(1)
         generation_oov_logits = torch.zeros(batch_size, self.max_oov_count)
+        if torch.cuda.is_available():
+            generation_oov_logits = generation_oov_logits.cuda()
         generation_logits = torch.cat([generation_logits, generation_oov_logits], dim=1)
         copy_logits = self.get_copy_score(encoder_output,
                                           src_tokens_with_oov,
@@ -112,10 +150,11 @@ class CopyTransformerDecoder(nn.Module):
         total_logit = torch.exp(generation_logits) + copy_logits
         total_prob = total_logit / torch.sum(total_logit, 1).unsqueeze(1)
         total_prob = torch.log(total_prob)
-        return total_prob, decoder_output.squeeze(1)
+        return total_prob, decoder_output.squeeze(1), copy_state, encoder_output, encoder_mask
 
     def get_attn_read_input(self, encoder_output, prev_context_state,
-                            prev_output_tokens, src_tokens_with_oov, src_max_len):
+                            prev_output_tokens, src_tokens_with_oov,
+                            src_max_len, prev_copy_state):
         """
         build CopyNet decoder input of "attentive read" part.
         :param encoder_output:
@@ -137,6 +176,8 @@ class CopyTransformerDecoder(nn.Module):
         input_copy_weight = torch.softmax(input_copy_logit.squeeze(2), 1)
         # B x 1 x H
         copy_state = torch.bmm(input_copy_weight.unsqueeze(1), encoder_output)
+        if prev_copy_state is not None:
+            copy_state = torch.cat([prev_copy_state, copy_state], dim=1)
         return copy_state
 
     def get_copy_score(self, encoder_out, src_tokens_with_oov, decoder_output, encoder_output_mask):
