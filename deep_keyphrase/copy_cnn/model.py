89453a75eacc9f0b7eb1178c69f5427e6859d25d
xiayubin <xiayubin@patsnap.com>
2019-12-05 18:44:21 +0800

    add copycnn module ,require to be implemented

---
+++ deep_keyphrase/copy_cnn/model.py
@@ -0,0 +1,20 @@
+# -*- coding: UTF-8 -*-
+import torch.nn as nn
+
+
+class CopyCnnModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self):
+        pass
+
+
+class CopyCnnEncoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+
+class CopyCnnDecoder(nn.Module):
+    def __init__(self):
+        super().__init__()
