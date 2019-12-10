8d82db1e5d2c63534c68c294d0b9a2fb759c4e66
xiayubin <xiayubin@patsnap.com>
2019-12-10 18:20:08 +0800

    update default learning rate

--- deep_keyphrase/copy_rnn/train.py
+++ deep_keyphrase/copy_rnn/train.py
@@ -160,7 +160,7 @@ class CopyRnnTrainer(BaseTrainer):
         parser.add_argument("-keyphrase_field", default='keyphrases', type=str, help='')
         parser.add_argument("-epochs", type=int, default=10, help='')
         parser.add_argument("-batch_size", type=int, default=64, help='')
-        parser.add_argument("-learning_rate", type=float, default=1e-3, help='')
+        parser.add_argument("-learning_rate", type=float, default=1e-4, help='')
         parser.add_argument("-eval_batch_size", type=int, default=50, help='')
         parser.add_argument("-dropout", type=float, default=0.1, help='')
         parser.add_argument("-grad_norm", type=float, default=0.0, help='')
