ce4538c09059206b568c9b5f642236400f7686b5
xiayubin <xiayubin@patsnap.com>
2019-12-18 12:07:10 +0800

    add new input data format

--- deep_keyphrase/evaluation.py
+++ deep_keyphrase/evaluation.py
@@ -1,38 +1,61 @@
 # -*- coding: UTF-8 -*-
+import copy
 from operator import itemgetter
 from collections import OrderedDict
-from pysenal import read_jsonline_lazy
+from pysenal import read_jsonline_lazy, read_json
 
 
 class KeyphraseEvaluator(object):
-    def __init__(self, top_n, metrics_mode):
+    def __init__(self, top_n,
+                 metrics_mode,
+                 token_field='tokens',
+                 true_keyphrase_field='keyphrases',
+                 pred_keyphrase_field='pred_keyphrases'):
         self.top_n = top_n
         self.metrics_mode = metrics_mode
+        self.token_field = token_field
+        self.true_keyphrase_field = true_keyphrase_field
+        self.pred_keyphrase_field = pred_keyphrase_field
 
-    def evaluate(self, src_filename, eval_mode='all'):
+    def __load_data(self, input_data):
+        if isinstance(input_data, str):
+            if input_data.endswith('.json'):
+                data_source = read_json(input_data)
+            elif input_data.endswith('jsonl'):
+                data_source = read_jsonline_lazy(input_data)
+            else:
+                raise ValueError('input file type is not supported, only support .json and .jsonl')
+        elif isinstance(input_data, list):
+            data_source = copy.deepcopy(input_data)
+        else:
+            raise TypeError('input data type error. only accept str (path) and  list.')
+        return data_source
+
+    def evaluate(self, input_data, eval_mode='all', ):
+        data_source = self.__load_data(input_data)
         if self.metrics_mode == 'micro':
-            result = self.evaluate_micro_average(src_filename, eval_mode)
+            result = self.evaluate_micro_average(data_source, eval_mode)
         elif self.metrics_mode == 'macro':
-            result = self.evaluate_macro_average(src_filename, eval_mode)
+            result = self.evaluate_macro_average(data_source, eval_mode)
         else:
             raise ValueError('evaluation mode is error.')
         return result
 
-    def evaluate_micro_average(self, src_filename, eval_mode):
+    def evaluate_micro_average(self, data_source, eval_mode):
         self.__check_eval_mode(eval_mode)
         top_counter = OrderedDict()
         for n in self.top_n:
             true_positive_count = 0
             pred_count = 0
             true_count = 0
-            for record in read_jsonline_lazy(src_filename):
-                tokens = record['tokens']
+            for record in data_source:
+                tokens = record[self.token_field]
                 true_positive_phrase_list = []
-                pred_phrase_list_topn = self.filter_phrase(record['pred_keyphrases'],
+                pred_phrase_list_topn = self.filter_phrase(record[self.pred_keyphrase_field],
                                                            eval_mode,
-                                                           record['tokens'],
+                                                           tokens,
                                                            n)
-                true_phrase_list = self.filter_phrase(record['keyphrases'], eval_mode, tokens)
+                true_phrase_list = self.filter_phrase(record[self.true_keyphrase_field], eval_mode, tokens)
                 for predict_phrase in pred_phrase_list_topn:
                     if predict_phrase in true_phrase_list:
                         true_positive_phrase_list.append(predict_phrase)
@@ -56,19 +79,19 @@ class KeyphraseEvaluator(object):
                               'f1': f1}
         return top_counter
 
-    def evaluate_macro_average(self, src_filename, eval_mode):
+    def evaluate_macro_average(self, data_source, eval_mode):
         self.__check_eval_mode(eval_mode)
         top_counter = OrderedDict()
         for n in self.top_n:
             counter = []
-            for record in read_jsonline_lazy(src_filename):
-                tokens = record['tokens']
+            for record in data_source:
+                tokens = record[self.token_field]
                 true_positive_topn_phrase_list = []
-                pred_phrase_list_topn = self.filter_phrase(record['pred_keyphrases'],
+                pred_phrase_list_topn = self.filter_phrase(record[self.pred_keyphrase_field],
                                                            eval_mode,
-                                                           record['tokens'],
+                                                           record[self.token_field],
                                                            n)
-                true_phrase_list = self.filter_phrase(record['keyphrases'], eval_mode, tokens)
+                true_phrase_list = self.filter_phrase(record[self.true_keyphrase_field], eval_mode, tokens)
                 for predict_phrase in pred_phrase_list_topn:
                     if predict_phrase in true_phrase_list:
                         true_positive_topn_phrase_list.append(predict_phrase)
