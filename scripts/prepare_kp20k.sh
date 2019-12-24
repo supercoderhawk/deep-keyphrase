74ff29e93dabcc0682b2926f7124f6df46109de2
xiayubin <xiayubin@patsnap.com>
2019-12-24 18:04:26 +0800

    update kp20k data preprocess script

--- scripts/prepare_kp20k.sh
+++ scripts/prepare_kp20k.sh
@@ -7,7 +7,11 @@ SRC_TEST=$DATA_DIR/raw/kp20k_new/kp20k_testing.json
 DEST_TRAIN=$DATA_DIR/kp20k.train.jsonl
 DEST_VALID=$DATA_DIR/kp20k.valid.jsonl
 DEST_TEST=$DATA_DIR/kp20k.test.jsonl
+DEST_VOCAB=$DATA_DIR/kp20k_vocab.txt
 
-python3 deep_keyphrase/data_process/preprocess.py -src_filename $SRC_TRAIN -dest_filename $DEST_TRAIN
-python3 deep_keyphrase/data_process/preprocess.py -src_filename $SRC_VALID -dest_filename $DEST_VALID
-python3 deep_keyphrase/data_process/preprocess.py -src_filename $SRC_TEST -dest_filename $DEST_TEST
+python3 deep_keyphrase/data_process/preprocess.py -src_filename $SRC_TRAIN \
+        -dest_filename $DEST_TRAIN -dest_vocab_path $DEST_VOCAB -src_lower -target_lower
+python3 deep_keyphrase/data_process/preprocess.py -src_filename $SRC_VALID \
+        -dest_filename $DEST_VALID -src_lower -target_lower
+python3 deep_keyphrase/data_process/preprocess.py -src_filename $SRC_TEST \
+        -dest_filename $DEST_TEST -src_lower -target_lower
