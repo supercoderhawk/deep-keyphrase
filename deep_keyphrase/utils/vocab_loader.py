762ac6d4055b2ce9c998b4474ef92d5a7fbdd451
xiayubin <xiayubin@patsnap.com>
2019-12-24 18:24:24 +0800

    load vocab func adjustment, not pre assign specific word

--- deep_keyphrase/utils/vocab_loader.py
+++ deep_keyphrase/utils/vocab_loader.py
@@ -4,17 +4,23 @@ from .constants import *
 
 
 def load_vocab(src_filename, vocab_size=None):
-    vocab2id = {PAD_WORD: 0,
-                UNK_WORD: 1,
-                BOS_WORD: 2,
-                EOS_WORD: 3,
-                DIGIT_WORD: 4,
-                SEP_WORD: 5}
+    vocab2id = {}
     for word in read_lines_lazy(src_filename):
         if word not in vocab2id:
             vocab2id[word] = len(vocab2id)
 
         if vocab_size and len(vocab2id) >= vocab_size:
             break
-
+    if PAD_WORD not in vocab2id:
+        raise ValueError('padding char is not in vocab')
+    if UNK_WORD not in vocab2id:
+        raise ValueError('unk char is not in vocab')
+    if BOS_WORD not in vocab2id:
+        raise ValueError('begin of sentence char is not in vocab')
+    if EOS_WORD not in vocab2id:
+        raise ValueError('end of sentence char is not in vocab')
+    if DIGIT_WORD not in vocab2id:
+        raise ValueError('digit char is not in vocab')
+    if SEP_WORD not in vocab2id:
+        raise ValueError('separator char is not in vocab')
     return vocab2id
