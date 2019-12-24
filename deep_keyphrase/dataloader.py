fb2fac052bd5e4779e20971265998e0bb6aa0629
xiayubin <xiayubin@patsnap.com>
2019-12-24 18:22:59 +0800

    parallel reading adjustment

--- deep_keyphrase/dataloader.py
+++ deep_keyphrase/dataloader.py
@@ -44,10 +44,11 @@ class KeyphraseDataLoader(object):
         self.max_oov_count = args.max_oov_count
         self.max_target_len = args.max_target_len
         self.mode = mode
-        self.pre_fetch = args.prefetch
-        self.shuffle_in_batch = args.shuffle_in_batch
+        self.prefetch = args.prefetch
+        self.shuffle = args.shuffle
         self.token_field = args.token_field
         self.keyphrases_field = args.keyphrase_field
+
         self.is_inference = mode != TRAIN_MODE
 
     def __iter__(self):
@@ -55,6 +56,8 @@ class KeyphraseDataLoader(object):
 
     def collate_fn(self, item):
         tokens = item[self.token_field]
+        if len(tokens) > self.max_src_len:
+            tokens = tokens[:self.max_src_len]
         token_ids_with_oov = []
         token_ids = []
         oov_list = []
@@ -68,7 +71,7 @@ class KeyphraseDataLoader(object):
                         token_ids_with_oov.append(self.vocab_size + self.max_oov_count - 1)
                     else:
                         token_ids_with_oov.append(self.vocab_size + len(oov_list))
-                        oov_list.append(oov_list)
+                        oov_list.append(token)
                 else:
                     token_ids_with_oov.append(self.vocab_size + oov_list.index(token))
             else:
@@ -88,7 +91,7 @@ class KeyphraseDataLoader(object):
             for token in keyphrase:
                 target_ids.append(self.vocab2id.get(token, self.vocab2id[UNK_WORD]))
             target_ids.append(self.vocab2id[EOS_WORD])
-            final_item[TARGET] = target_ids
+            final_item[TARGET] = target_ids[:self.max_target_len]
         return final_item
 
 
@@ -97,14 +100,10 @@ class KeyphraseDataIterator(object):
         self.loader = loader
         self.data_source = loader.data_source
         self.batch_size = loader.batch_size
-        self.num_workers = multiprocessing.cpu_count()
-        # !!! important !!!
-        # set torch num thread for 1 to avoid deadlocking when call torch.tensor in child process
-        # reference: https://github.com/pytorch/pytorch/issues/17199#issuecomment-465313245
-        torch.set_num_threads(1)
+        self.num_workers = multiprocessing.cpu_count() // 2 or 1
 
         if self.loader.mode == TRAIN_MODE:
-            self.chunk_size = self.num_workers * 10
+            self.chunk_size = self.num_workers * 100
         else:
             self.chunk_size = self.batch_size
         self._data = self.load_data(self.chunk_size)
@@ -113,7 +112,7 @@ class KeyphraseDataIterator(object):
         self.workers = []
         self.worker_shutdown = False
 
-        if self.loader.mode == TRAIN_MODE:
+        if self.loader.mode in {TRAIN_MODE, EVAL_MODE}:
             self.input_queue = multiprocessing.Queue(-1)
             self.output_queue = multiprocessing.Queue(-1)
             self.done_event = multiprocessing.Event()
@@ -130,6 +129,8 @@ class KeyphraseDataIterator(object):
     def __iter__(self):
         if self.loader.mode == TRAIN_MODE:
             yield from self.iter_train()
+        elif self.loader.mode == EVAL_MODE:
+            yield from self.iter_inference_parallel()
         else:
             yield from self.iter_inference()
 
@@ -153,35 +154,71 @@ class KeyphraseDataIterator(object):
             if raw_batch is None:
                 break
             try:
-                batch = []
-                for item in raw_batch:
-                    if self.loader.args.processed:
-                        batch.append(item)
-                    else:
-                        batch.append(self.loader.collate_fn(item))
-                if self.loader.shuffle_in_batch:
-                    random.shuffle(batch)
-
-                if self.loader.is_inference:
-                    batch = self.padding_batch_inference(batch)
-                else:
-                    batch = self.padding_batch_train(batch)
+                batch = self.padding_batch(raw_batch)
 
                 self.output_queue.put(batch)
 
             except Exception as e:
                 self.output_queue.put(ExceptionWrapper(sys.exc_info()))
 
+    def padding_batch(self, raw_batch):
+        max_src_len = self.loader.max_src_len
+        max_target_len = self.loader.max_target_len
+        pad_id = self.loader.vocab2id[PAD_WORD]
+        token_ids_list = []
+        token_len_list = []
+        token_oov_ids_list = []
+        oov_len_list = []
+        oov_list = []
+        raw_item_list = []
+        target_ids_list = []
+
+        for raw_item in raw_batch:
+            if not self.loader.args.processed:
+                item = self.loader.collate_fn(raw_item)
+            else:
+                item = raw_item
+            token_len = len(item[self.loader.token_field])
+            token_len_list.append(token_len)
+            token_ids = item[TOKENS] + [pad_id] * (max_src_len - token_len)
+            token_ids_list.append(token_ids)
+            token_oov_ids = item[TOKENS_OOV] + [pad_id] * (max_src_len - token_len)
+            token_oov_ids_list.append(token_oov_ids)
+            oov_len_list.append(item[OOV_COUNT])
+            oov_list.append(item[OOV_LIST])
+            if self.loader.is_inference:
+                raw_item_list.append(raw_item)
+            else:
+                target_ids = item[TARGET] + [pad_id] * (max_target_len + 1 - len(item[TARGET]))
+                target_ids_list.append(target_ids)
+        token_ids_np = np.array(token_ids_list, dtype=np.long)
+        token_len_np = np.array(token_len_list, dtype=np.long)
+        token_oov_np = np.array(token_oov_ids_list, dtype=np.long)
+        oov_len_np = np.array(oov_len_list, dtype=np.long)
+        batch = {TOKENS: token_ids_np,
+                 TOKENS_OOV: token_oov_np,
+                 TOKENS_LENS: token_len_np,
+                 OOV_COUNT: oov_len_np,
+                 OOV_LIST: oov_list}
+        if not self.loader.is_inference:
+            batch[TARGET] = np.array(target_ids_list, dtype=np.long)
+        else:
+            batch[RAW_BATCH] = raw_item_list
+
+        return batch
+
     def __prefetch(self):
         if self.loader.mode == TRAIN_MODE:
             item_chunk = next(self._data)
+            if self.loader.shuffle:
+                random.shuffle(item_chunk)
             batches, redundant_batch = self.get_batches(item_chunk, [])
             self._redundant_batch = redundant_batch
             for batch in batches:
                 self.input_queue.put(batch)
                 self._batch_count_in_output_queue += 1
         else:
-            for _ in range(self.num_workers * 100):
+            for _ in range(self.num_workers * 5):
                 try:
                     item_chunk = next(self._data)
                 except StopIteration:
@@ -192,13 +229,15 @@ class KeyphraseDataIterator(object):
     def iter_train(self):
         redundant_batch = self._redundant_batch
         for item_chunk in self._data:
+            if self.loader.shuffle:
+                random.shuffle(item_chunk)
             batches, redundant_batch = self.get_batches(item_chunk, redundant_batch)
             batch_idx = 0
             for idx in range(self._batch_count_in_output_queue):
                 if batch_idx < len(batches):
                     self.input_queue.put(batches[batch_idx])
                     batch_idx += 1
-                yield self.output_queue.get()
+                yield self.batch2tensor(self.output_queue.get())
 
             if batch_idx < len(batches):
                 for batch in batches[batch_idx:]:
@@ -211,7 +250,7 @@ class KeyphraseDataIterator(object):
 
         if self._batch_count_in_output_queue:
             for idx in range(self._batch_count_in_output_queue):
-                yield self.output_queue.get()
+                yield self.batch2tensor(self.output_queue.get())
 
     def get_batches(self, item_chunk, batch):
         if self.loader.args.processed:
@@ -226,7 +265,6 @@ class KeyphraseDataIterator(object):
         return batches, []
 
     def get_batches_raw(self, item_chunk, batch):
-
         batches = []
 
         for item in item_chunk:
@@ -271,14 +309,14 @@ class KeyphraseDataIterator(object):
         assert self.workers
         for item_chunk in self._data:
             if self._batch_count_in_output_queue > 0:
-                yield self.output_queue.get()
+                yield self.batch2tensor(self.output_queue.get())
                 self.input_queue.put(item_chunk)
             else:
                 self.input_queue.put(item_chunk)
-                yield self.output_queue.get()
+                yield self.batch2tensor(self.output_queue.get())
         if self._batch_count_in_output_queue:
             for _ in range(self._batch_count_in_output_queue):
-                yield self.output_queue.get()
+                yield self.batch2tensor(self.output_queue.get())
 
     def iter_inference(self):
         assert not self._batch_count_in_output_queue
@@ -288,61 +326,16 @@ class KeyphraseDataIterator(object):
             item_chunk = [self.loader.collate_fn(item) for item in item_chunk]
             if len(item_chunk) > 1:
                 item_chunk = self.reorder_batch(item_chunk)
-            yield self.padding_batch_inference(item_chunk)
+            yield self.batch2tensor(self.padding_batch(item_chunk))
 
-    def padding_batch_train(self, batch):
-        name2max_len = {TOKENS: self.loader.max_src_len,
-                        TOKENS_OOV: self.loader.max_src_len,
-                        TARGET: self.loader.max_target_len + 1}
-        result = {}
-        for key in batch[0].keys():
-            data = [b[key] for b in batch]
-            if key in name2max_len:
-                pad_data, pad_data_len = self.__padding(data, name2max_len[key])
-                if key == TOKENS:
-                    src_tensor = torch.tensor(pad_data_len, dtype=torch.int64)
-                    result[TOKENS_LENS] = src_tensor
+    def batch2tensor(self, batch):
+        new_batch = {}
+        for key, val in batch.items():
+            if isinstance(val, np.ndarray):
+                new_batch[key] = torch.as_tensor(val)
             else:
-                pad_data = torch.tensor(data, dtype=torch.int64)
-            result[key] = pad_data
-
-        return result
-
-    def padding_batch_inference(self, batch):
-        name2max_len = {TOKENS: self.loader.max_src_len,
-                        TOKENS_OOV: self.loader.max_src_len}
-        padding_key = {TOKENS, TOKENS_OOV, OOV_COUNT}
-        result = {}
-        for key in batch[0].keys():
-            data = [b[key] for b in batch]
-            if key in padding_key:
-                if key in name2max_len:
-                    pad_data, pad_data_len = self.__padding(data, name2max_len[key])
-                    if key == TOKENS:
-                        src_tensor = torch.tensor(pad_data_len, dtype=torch.int64)
-                        result[TOKENS_LENS] = src_tensor
-                else:
-                    pad_data = torch.tensor(data, dtype=torch.int64)
-                result[key] = pad_data
-            else:
-                result[key] = data
-        result[RAW_BATCH] = [i[RAW_BATCH] for i in batch]
-        return result
-
-    def __padding(self, x_raw, max_len):
-        x_raw = np.asarray(x_raw)
-        x_len_list = []
-        for x_ in x_raw:
-            x_len = len(x_)
-            if x_len > max_len:
-                x_len_list.append(max_len)
-            else:
-                x_len_list.append(x_len)
-
-        pad_id = self.loader.vocab2id[PAD_WORD]
-        x = np.array([np.concatenate((x_[:max_len], [pad_id] * (max_len - len(x_)))) for x_ in x_raw])
-        x = torch.tensor(x, dtype=torch.int64)
-        return x, x_len_list
+                new_batch[key] = val
+        return new_batch
 
     def _shutdown_workers(self):
         if not self.worker_shutdown and self.loader.mode in {TRAIN_MODE}:
