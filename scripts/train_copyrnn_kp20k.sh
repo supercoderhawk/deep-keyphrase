bd4f3a785c1cfff1c56ff9ea28ab3ad5a442b199
xiayubin <xiayubin@patsnap.com>
2020-02-21 09:26:04 +0800

    adjust kp20k train hyperparameters

--- scripts/train_copyrnn_kp20k.sh
+++ scripts/train_copyrnn_kp20k.sh
@@ -13,6 +13,8 @@ EXP_NAME=copyrnn_kp20k_basic
 python3 deep_keyphrase/copy_rnn/train.py -exp_name $EXP_NAME \
   -train_filename $TRAIN_FILENAME \
   -valid_filename $VALID_FILENAME -test_filename $TEST_FILENAME \
+  -batch_size 128 -max_src_len 1500 -learning_rate 1e-3 \
   -token_field title_and_abstract_tokens -keyphrase_field keyword_tokens \
   -vocab_path $VOCAB_PATH -dest_base_dir $DEST_DIR \
-  -bidirectional -teacher_forcing -copy_net -shuffle -prefetch
+  -bidirectional -teacher_forcing -copy_net -shuffle -prefetch \
+  -schedule_lr -schedule_step 10000
